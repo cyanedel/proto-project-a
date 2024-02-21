fc5b844ed272872543fa6113f9ac71e26a693eaa
cyanedel <cleewlkr@gmail.com>
2024-02-21 17:52:22 +0700

    Basic features;

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { useEffect, useState } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import { icons } from './component/icons';
 import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+
+import { ReactComponent as IconHat } from './assets/hat.svg';
+
+import { ReactComponent as IconFlame } from './assets/flames.svg';
+import { ReactComponent as IconWater } from './assets/water.svg';
+import { ReactComponent as IconLeaf } from './assets/leaf.svg';
+import { ReactComponent as IconStone } from './assets/stone.svg';
+import { ReactComponent as IconMedal } from './assets/gold-medal.svg';
+import { ReactComponent as IconDiamond } from './assets/diamond.svg';
+import { ReactComponent as IconRocket } from './assets/shuttle.svg';
+import { ReactComponent as IconRabbit } from './assets/rabbit.svg';
+import { ReactComponent as IconDragon } from './assets/dragon.svg';
+import { ReactComponent as IconPoop } from './assets/poop.svg';
+import { ReactComponent as IconZeus } from './assets/zeus.svg';
 
 function App() {
   const [cardNum, setCardNum] = useState(0);
   const [cardList, setCardList] = useState([]);
-  // const [cardStates, setCardStates] = useState([])
+  const [cardStatelist, setCardStatelist] = useState([])
 
   function generateOptions(nMin, nMax){
     let a = []
@@ -35,34 +47,52 @@ function App() {
   }
 
   useEffect(()=>{
-    const variants = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const base = variants.slice(0,cardNum/2)
+    const variants = [
+        [1, <IconFlame width="64" height="64" />]
+      , [2, <IconWater width="64" height="64" />]
+      , [3, <IconStone width="64" height="64" />]
+      , [4, <IconLeaf width="64" height="64" />]
+      , [5, <IconDiamond width="64" height="64" />]
+      , [6, <IconRabbit width="64" height="64" />]
+      , [7, <IconPoop width="64" height="64" />]
+      , [8, <IconDragon width="64" height="64" />]
+      , [9, <IconRocket width="64" height="64" />]
+      , [10, <IconMedal width="64" height="64" />]
+      , [11, <IconZeus width="64" height="64" />]
+    ]
+    const shuffledVariants = shuffle(variants)
+    const base = shuffledVariants.slice(0,cardNum/2)
     const dups = base.concat(base)
     const shuffled = shuffle(dups)
-    const shuffledWithState = shuffled.map((item)=>{return [item, 0]})
-    setCardList(shuffledWithState)
+    // const defaultCardState = Array(cardNum).fill(false)
+    // const shuffledWithState = shuffled.map((item)=>{return [item, false]})
+    setCardList(shuffled)
+    setCardStatelist(Array(cardNum).fill(false))
   }, [cardNum])
 
-  function RenderCards(){
-    return cardList.map((item, index)=>{
-      return (
-        <Card key={index} className="m-1 p-3 d-flex align-items-center justify-content-center" style={{height: 120, width: 120}}>
-          {item[0]}, {item[1]}
-        </Card>
-        )
-    })
+  function handleCardClick(cardIndex){
+    setCardStatelist(prevState => prevState.map((item, index)=>{
+      return index === cardIndex ? !item : item
+    }))
   }
 
   return (
     <div className="App">
-      <Container className='my-5' style={{maxWidth: 720}}>
+      <Container className='my-5 mw-1'>
+        {/* <div>{cardList}</div> */}
         <div className='mb-3'>Playing with: {cardNum} cards</div>
         <Row className='g-2 align-items-center justify-content-center'>
-          {cardNum ? <RenderCards /> : <></>}
+          {cardList.map((item, index)=>{
+            return (
+              <Card key={index} className={`m-1 p-3 d-flex shadow ${cardStatelist[index] ? "flip": ""}`} onClick={()=>handleCardClick(index)}>
+                <div className='card-front bg-light text-black'>{cardStatelist[index] ? item[1] : ""}</div>
+                <div className='card-back bg-dark'><IconHat width="64" height="64" className='fill-white' /></div>
+              </Card>
+              )
+          })}
         </Row>
-        
       </Container>
-      <Container style={{maxWidth: 480}} className='my-5'>
+      <Container className='my-5 mw-2'>
         <div className='mb-3'>Select Number of Cards to Play</div>
         <Row xs={3} className='g-2'>
           <RenderButtons optMin={4} optMax={20} />
